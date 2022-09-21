803cf0540cd4374525ceec5aeba1188a34f39d01
koreyp89 <koreypickering341@gmail.com>
2022-09-20 21:05:53 -0500

    cleaned up the code and created argmax function

--- main.py
+++ main.py
@@ -1,62 +1,95 @@
 import numpy as np
 import pandas as pd
 import sys
+import math
 
 
-def make_test_set():
-    training_data = pd.read_csv(sys.argv[1], sep="\t", header=[0])
-    print(training_data.head())
-    global COLUMN_HEADERS
-    COLUMN_HEADERS = training_data.columns
+def make_test_set(file_location):
+    training_data = pd.read_csv(file_location, sep="\t", header=[0])
     return training_data
 
 
+class Bayes_reasoner:
+
+    def __init__(self, training_file_location):
+        self.matrix = make_test_set(training_file_location)
+        self.training_set = self.matrix.values
+        self.attributes = self.matrix.columns[:len(self.matrix.columns)-1]
+        self.class_priors = {}
+        self.initialize_class_priors()
+        self.get_class_priors()
+        self.conditional_probabilities = {}
+        self.initialize_conditionals()
+        self.get_conditionals()
+        self.print_conditionals()
+
+    def initialize_conditionals(self):
+        for attribute in self.attributes:
+            self.conditional_probabilities[attribute] = {}
+            for j in range(2):
+                self.conditional_probabilities[attribute][j] ={}
+                for i in range(2):
+                    self.conditional_probabilities[attribute][j][i] = 0
+
+    def argmax_classes(self, example):
+        argmax = 0
+        argmax_value = 0
+        for x in range(2):
+            summation = math.log(self.class_priors[x], 2)
+            for i in range(self.attributes):
+                summation += math.log(self.conditional_probabilities[self.attributes[i]][example[i]][x])
+            if summation > argmax_value:
+                argmax_value = summation
+                argmax = x
+        return argmax
+
+    def get_conditionals(self):
+        denom = len(self.training_set[:,-1])
+        p_of_0 = self.class_priors[0]
+        p_of_1 = self.class_priors[1]
+        for i in range(len(self.attributes)):
+            attribute0_and_class0 = 0
+            attribute1_and_class0 = 0
+            attribute1_and_class1 = 0
+            attribute0_and_class1 = 0
+            for j in range(len(self.training_set[:,-1])):
+                if self.training_set[j][i] == 0:
+                    if self.training_set[j][-1] == 0:
+                        attribute0_and_class0 += 1
+                    else:
+                        attribute0_and_class1 += 1
+                else:
+                    if self.training_set[j][-1] == 0:
+                        attribute1_and_class0 += 1
+                    else:
+                        attribute1_and_class1 += 1
+            self.conditional_probabilities[self.attributes[i]][0][0] = (attribute0_and_class0/(denom*p_of_0))
+            self.conditional_probabilities[self.attributes[i]][0][1] = (attribute0_and_class1/(denom*p_of_1))
+            self.conditional_probabilities[self.attributes[i]][1][0] = (attribute1_and_class0/(denom*p_of_0))
+            self.conditional_probabilities[self.attributes[i]][1][1] = (attribute1_and_class1/(denom*p_of_1))
+
+    def get_class_priors(self):
+        zero_total = 0
+        for i in self.training_set[: , -1]:
+            zero_total += 1 if i == 0 else 0
+        self.class_priors[0] = zero_total/len(self.training_set[: , -1])
+        self.class_priors[1] = 1 - self.class_priors[0]
+
+    def initialize_class_priors(self):
+        self.class_priors[0] = 0
+        self.class_priors[1] = 0
+
+    def print_conditionals(self):
+        print("P(class=0)=" + str(round(self.class_priors[0], 2)), end=' ')
+        for att in self.attributes:
+            for i in range(2):
+                print("P(" + att + "=" + str(i) + "|0)=" + str.format("{:.2f}", self.conditional_probabilities[att][i][0]), end=' ')
+        print()
+        print("P(class=1)=" + str(round(self.class_priors[1], 2)), end=' ')
+        for att in self.attributes:
+            for i in range(2):
+                print("P(" + att + "=" + str(i) + "|1)=" + str.format("{:.2f}", self.conditional_probabilities[att][i][1]), end=' ')
+
 
 if __name__ == "__main__":
-    training_dat = make_test_set()
-    training_data = training_dat.values
-
-    class_column = training_data[:, -1]
-    unique_val, counts = np.unique(class_column, return_counts=True)
-
-    total = counts[0] + counts[1]
-    if unique_val[0] == 0:
-        class_0 = counts[0]
-        class_prob_0 = counts[0] / total
-    else:
-        class_0 = counts[1]
-        class_prob_0 = counts[1] / total
-
-    class_1 = total - class_0
-    class_prob_1 = 1 - class_prob_0
-    rows, columns = training_data.shape
-    conditionals = {}
-
-    for header in COLUMN_HEADERS:
-        conditionals[header] = {}
-        conditionals[header][0] = {}
-        conditionals[header][1] = {}
-    for j in range(columns):
-        attribute_and_class0 = 0
-        attribute_and_class1 = 0
-        for i in range(rows):
-            if training_data[i][j] == 1:
-                if class_column[i] == 1:
-                    attribute_and_class1 += 1
-                else:
-                    attribute_and_class0 += 1
-        conditionals[COLUMN_HEADERS[j]][1][1] = attribute_and_class1 / class_1
-        conditionals[COLUMN_HEADERS[j]][1][0] = attribute_and_class0 / class_0
-        conditionals[COLUMN_HEADERS[j]][0][1] = 1 - conditionals[COLUMN_HEADERS[j]][1][1]
-        conditionals[COLUMN_HEADERS[j]][0][0] = 1 - conditionals[COLUMN_HEADERS[j]][1][0]
-    print("P(class=0)=" + str(round(class_prob_0, 2)), end=' ')
-    for header in COLUMN_HEADERS[:len(COLUMN_HEADERS) - 1]:
-        for i in range(2):
-            print("P(" + header + "=" + str(i) + "|0)=" + str.format("{:.2f}", conditionals[header][i][0]), end=' ')
-    print()
-    print("P(class=1)=" + str(round(class_prob_1, 2)), end=' ')
-    for header in COLUMN_HEADERS[:len(COLUMN_HEADERS) - 1]:
-        for i in range(2):
-            print("P(" + header + "=" + str(i) + "|1)=" + str.format("{:.2f}", conditionals[header][i][1]), end=' ')
-    attributes = COLUMN_HEADERS[:len(COLUMN_HEADERS) - 1]
-
+    Bayes_reasoner(sys.argv[1])
